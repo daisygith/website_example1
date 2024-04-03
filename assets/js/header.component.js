77d04ceca9628d31932e5d42738dbc97104fddc8
daisygith <daisygith@users.noreply.github.com>
2024-04-03 07:30:39 +0200

    changed header

--- assets/js/header.component.js
+++ assets/js/header.component.js
@@ -5,24 +5,32 @@ class Header extends HTMLElement {
             <header class="header">
                 <div class="site-header">
                 <!--                    #header-desktop-open-->
-                        <div class="element-widget-container">
+                        <div class="header-content">
                             <a href="/" class="logo">  <!-- "http://h-tx.pl/"-->
                                <img src="../images/LOGO_b.png" alt="Heating Technix">
         <!--                        HEATING TECHNIX-->
                             </a>
+<!--                        </div>-->
+<!--                        <div class="menu-elements">-->
+<!--                            <div class="bx bx-menu" id="menu-icon"><span class="animate" style="&#45;&#45;i:2;"></div>-->
+<!--                            <div class="element-widget-container">-->
+                                <nav id="site-navigation" class="main-navigation">
+                                    <ul id="primary-menu" class="menu">
+                                        <li id="menu-item-portfolio" class="nav-item"><a href="/portfolio">Portfolio</a></li>
+                                        <li id="menu-item-offer" class="nav-item"><a href="/offer">Oferta</a></li>
+                                        <li id="menu-item-about" class="nav-item"><a href="/about">O nas</a></li>
+                                        <li id="menu-item-contact" class="nav-item"><a href="/contact">Kontakt</a></li>
+                                    </ul>
+                                </nav>
+<!--                            </div>-->
+                            
+                            <div class="hamburger">
+                                <div class="bar"></div>
+                                <div class="bar"></div>
+                                <div class="bar"></div>
+                            </div>                 
                         </div>
-                        <div class="element-widget-container">
-                        <div class="bx bx-menu" id="menu-icon"><span class="animate" style="--i:2;"></div>
-                            <nav id="site-navigation" class="main-navigation">
-                                <ul id="primary-menu" class="menu">
-                                    <li id="menu-item-portfolio" class="menu-item menu-item-portfolio"><a href="/portfolio">Portfolio</a></li>
-                                    <li id="menu-item-offer" class="menu-item menu-item-offer"><a href="/offer">Oferta</a></li>
-                                    <li id="menu-item-about" class="menu-item menu-item-about"><a href="/about">O nas</a></li>
-                                    <li id="menu-item-contact" class="menu-item menu-item-contact"><a href="/contact">Kontakt</a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
+                </div>
             </header>
     `;
     }
