d309ea1c5c5a2b494a5412a68f6cf1491e41a95b
daisygith <daisygith@users.noreply.github.com>
2024-06-10 12:54:15 +0200

    changed header

--- assets/js/header.component.js
+++ assets/js/header.component.js
@@ -6,14 +6,10 @@ class Header extends HTMLElement {
                 <div class="site-header">
                 <!--                    #header-desktop-open-->
                         <div class="header-content">
-                            <a href="/" class="logo active" id="logo-id">  <!-- "http://h-tx.pl/"-->
+                            <a href="/" class="logo active" id="logo-id"> 
                                <img src="../images/LOGO_b.png" alt="Heating Technix">
         <!--                        HEATING TECHNIX-->
                             </a>
-<!--                        </div>-->
-<!--                        <div class="menu-elements">-->
-<!--                            <div class="bx bx-menu" id="menu-icon"><span class="animate" style="&#45;&#45;i:2;"></div>-->
-<!--                            <div class="element-widget-container">-->
                                 <nav id="site-navigation" class="main-navigation">
                                     <ul id="primary-menu" class="menu">
                                         <li id="menu-item-portfolio" class="nav-item" data-text="portfolio"><a href="/portfolio">Portfolio</a></li>
@@ -22,8 +18,6 @@ class Header extends HTMLElement {
                                         <li id="menu-item-contact" class="nav-item" data-text="contact"><a href="/contact">Kontakt</a></li>
                                     </ul>
                                 </nav>
-<!--                            </div>-->
-                            
                             <div class="hamburger">
                                 <div class="bar"></div>
                                 <div class="bar"></div>
